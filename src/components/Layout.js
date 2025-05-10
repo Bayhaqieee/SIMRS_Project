76237799152d032ad8e3bc58c7686ea153a83d69
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2025-05-10 22:30:20 +0700

    Header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,28 @@
-// Layout.js
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 const Layout = ({ children, userRole, onLogout }) => {
   const location = useLocation();
-
-  // Check if the current route is login page
   const isLoginPage = location.pathname === "/";
 
   return (
     <div className="App">
-      {/* Show sidebar only when not on login */}
+      {/* Sidebar */}
       {!isLoginPage && <Sidebar userRole={userRole} onLogout={onLogout} />}
-
-      {/* Apply margin only when sidebar is visible */}
+      
       <div
         className="content"
         style={{ marginLeft: !isLoginPage ? "16rem" : "0" }}
       >
-        <div className="main-content">{children}</div>
+        {/* Header */}
+        {!isLoginPage && <Header userRole={userRole} />}
+
+        {/* Main content */}
+        <div className="main-content pt-16 px-4 bg-gray-50 min-h-screen">
+          {children}
+        </div>
       </div>
     </div>
   );
